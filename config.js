f85d4d23a61533f271b192272ed61da2eeca7129
Grobler96 <grobleroliver@gmail.com>
2026-06-17 17:43:55 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.ATAM_GO_CONFIG = {
   SUPABASE_ANON_KEY: "sb_publishable_4Ii8Z8bGgQ5OrSKB2at_GA_GubBsWC1",
 
   WEBHOOKS: {
-    refreshDecoData: "",
+    refreshDecoData: "https://atamcpi.app.n8n.cloud/webhook/atam-go-action",
     recountToday: "",
     sendDailySummary: "",
     sendManagerAlert: "",
